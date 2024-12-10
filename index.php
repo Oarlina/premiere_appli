1e54d1386ce11bfb5644c6e95da9ff994d1fec1f
Rachel Marquant <marquantrachel@gmail.com>
2024-12-10 11:58:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Premiere Appli</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post"> <!-- creer un formulaire qui sera dans traitement et qui transmet les information via un formulaire -->
+        <p>
+            <label> <!-- premiere legende du formulaire-->
+                Nom du produit :
+                <input type="text" name="nom"> <!-- Zone ou remplir le nom du produit -->
+            </label>
+        </p>
+        <p>
+            <label>
+                Prix du produit : 
+                <input type="text" name="prix" step="any"> 
+            </label>
+        </p>
+        <p>
+            <label>
+                Quantité désirée : 
+                <input type="text" name="qtt" value="1">
+            </label>
+        </p>
+        <p>
+            <input type="submit" name="envoyer" value="Ajouter le produit"> <!-- boutton pour ajouter le produit -->
+        </p>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+<?php
+
+session_start();
+
+if (isset($_POST['envoyer']))
+{
+
+}
+// header("Location:index.php");
+
+
+
+
+
+?>
