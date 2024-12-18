a25a6a363302435f4cbd7309db806f680194b2e6
Rachel Marquant <marquantrachel@gmail.com>
2024-12-18 18:54:04 +0100

    Amelioration des messages d'alerte

--- traitement.php
+++ traitement.php
@@ -35,13 +35,13 @@ if (isset($_GET['action'])) // si le get action est pas vide
         case "delete":
             $index = $_GET['id']; // on crée  la valeur de l'indentation du tableau a supprimer
             $nom = $_GET['name']; // on cree la variable qui contiendra le nom du produit pour pouvoir l'afficher
-            $_SESSION['message']= "<p>$nom a ete supprimé</p>"; // on affiche que le produit a ete supprimer et on dit lequel
+            $_SESSION['message']= "<<p class='alerte'>$nom a ete supprimé</p>"; // on affiche que le produit a ete supprimer et on dit lequel
             UNSET($_SESSION['produits'][$index]); // on supprime le produit du tableau 
             header("Location:recap.php"); // c'est une redirection a la page index
             break;
         case "clear":
             UNSET($_SESSION['produits']); // on vide le tableau
-            $_SESSION['message']= "<p>Tout a ete supprimé</p>"; // on affiche que le tableau a ete vider
+            $_SESSION['message']= "<p class='alerte'>Tout a ete supprimé</p>"; // on affiche que le tableau a ete vider
             header("Location:recap.php"); // c'est une redirection a la page index
             break;
 
